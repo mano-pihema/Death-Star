8eac0292bb8c89810b856489bde163f982b15c60
Mano <m.raankin@gmail.com>
2022-06-10 10:13:58 +1200

    db buses list and home route

--- db.js
+++ db.js
@@ -4,6 +4,13 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-module.exports = {}
+module.exports = {
+  getAllBuses
+}
 
 //write functions below
+
+function getAllBuses(db =connection){
+  return db('buses').select()
+  
+}
--- routes/routes.js
+++ routes/routes.js
@@ -6,3 +6,13 @@ const router = express.Router()
 module.exports = router
 
 // define routes below
+
+router.get('/', async(req,res)=>{
+  try {
+    
+    const bus = await db.getAllBuses()
+    res.render('home', bus)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
