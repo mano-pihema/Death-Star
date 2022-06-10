241f1fb41c60cb82c0e5be66270c9ccfb9cd69c7
Ciara <cduncumnz@gmail.com>
2022-06-10 13:31:50 +1200

    formatted schedule and added descriptions

--- seeds/buses.js
+++ seeds/buses.js
@@ -5,27 +5,31 @@ exports.seed = async function (knex) {
     {
       id: 1,
       name: 'Death Star',
-      description: 'This is a Death Star description',
+      description:
+        "You've always wanted a go on one of these things. Don't even try deny it. ",
     },
     {
       id: 2,
       name: 'Halloween',
-      description: 'This is a Halloween description',
+      description: 'Only the spookiest bus you ever did see.',
     },
     {
       id: 3,
       name: 'Princess Party',
-      description: 'This is a Princess Party description',
+      description:
+        "Who doesn't love princesses? This bus comes fully loaded with half a dozen girls in frilly dresses. ",
     },
     {
       id: 4,
       name: 'Harry Potter',
-      description: 'This is a Harry Potter description',
+      description:
+        'All aboard the hogbus express! Includes butterbeer on tap and house elves(paid living wage) to serve canapes.',
     },
     {
       id: 5,
       name: 'Stripper Pole',
-      description: 'This is a Stripper Pole description',
+      description:
+        'Would it even be a party bus without a stripper pole? Show off your flexibilty and abandon your dignity all at the same time.',
     },
   ])
 }
